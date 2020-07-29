93531f8063dc377bd2d857d1ead60398bfd2488f
liaozq <810443336@qq.com>
2020-07-29 21:23:25 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/jianfa/Bixie.java
@@ -0,0 +1,12 @@
+package com.atguigu.jianfa;
+
+/**
+ * @author LiaoZQ
+ * @version 1.0
+ * @date 2020/7/29 21:19
+ */
+public class Bixie {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
